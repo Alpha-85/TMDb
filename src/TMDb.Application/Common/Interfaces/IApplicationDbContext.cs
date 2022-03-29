6bb5cf97a05bb0e5f5a24e0c20f5191f7f26d1b6
Alpha-85 <ljungdahlt@gmail.com>
2022-03-29 19:43:07 +0200

    DbSets for entities

--- src/TMDb.Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/TMDb.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,5 +1,13 @@
-﻿namespace TMDb.Application.Common.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using TMDb.Domain.Entities;
+
+namespace TMDb.Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
+    DbSet<Movie>? Movies { get; }
+    DbSet<Actor>? Actors { get; }
+    DbSet<Genre>? Genres { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
--- src/TMDb.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/TMDb.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using TMDb.Application.Common.Interfaces;
+using TMDb.Domain.Entities;
 
 namespace TMDb.Infrastructure.Persistence;
 
 public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Movie>? Movies { get; set; }
+    public DbSet<Actor>? Actors { get; set; }
+    public DbSet<Genre>? Genres { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
