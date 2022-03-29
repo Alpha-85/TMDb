cfbac69fcefcac75c3e25e8d9efda51de360ae1f
Alpha-85 <ljungdahlt@gmail.com>
2022-03-29 19:04:38 +0200

    applicationDbContext

---
+++ src/TMDb.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace TMDb.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+}
