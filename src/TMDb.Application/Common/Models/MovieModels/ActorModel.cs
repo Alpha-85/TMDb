0c207762d39b1b668c45a6da328b920b61d61e2e
Alpha-85 <ljungdahlt@gmail.com>
2022-03-29 21:02:42 +0200

    Models for movie and request, added controller

---
+++ src/TMDb.Application/Common/Models/MovieModels/ActorModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TMDb.Application.Common.Models.MovieModels;
+
+public class ActorModel
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
---
+++ src/TMDb.WebApi/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TMDb.Application.Common.Models.RequestModels;
+
+namespace TMDb.WebApi.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class MovieController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public MovieController(IMediator mediator)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create(MovieRequestModel request)
+    {
+        throw new NotImplementedException();
+    }
+}
